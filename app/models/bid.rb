0fce268a9191f9a69bee1f7cb99898778b472758
Jack Simmonds <js00928@surrey.ac.uk>
2016-12-01 17:02:25 +0000

    Changed the models to add presence validation for bids and has_many relationship for items

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,3 +1,4 @@
 class Bid < ActiveRecord::Base
   belongs_to :item
+  validates :bidder, :item, :value, presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
+	has_many :bids
 	validates :title, presence: true
 	validates :description, presence: true
 	validates :seller, presence: true
